27742c8f0e25e800a93054dd0a9380925136cbee
ngame <ngame@DESKTOP-HSA4VI4>
2018-01-14 13:47:47 +0330

    -Added Place Add API

--- GoogleMapsSDKUWP/PlaceHelper.cs
+++ GoogleMapsSDKUWP/PlaceHelper.cs
@@ -275,4 +275,49 @@ namespace GMapsUWP.Place
         }
 
     }
+
+    class Add
+    {
+        public static async Task<Response> AddPlace(Rootobject PlaceInfo)
+        {
+            try
+            {
+                var http = new HttpClient();
+                var r = await http.PostAsync($"https://maps.googleapis.com/maps/api/place/add/json?key={Initializer.GoogleMapAPIKey}", new StringContent(JsonConvert.SerializeObject(PlaceInfo)));
+                return JsonConvert.DeserializeObject<Response>((await r.Content.ReadAsStringAsync()));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public class Rootobject
+        {
+            public Location location { get; set; }
+            public int accuracy { get; set; }
+            public string name { get; set; }
+            public string phone_number { get; set; }
+            public string address { get; set; }
+            public string[] types { get; set; }
+            public string website { get; set; }
+            public string language { get; set; }
+        }
+
+        public class Location
+        {
+            public float lat { get; set; }
+            public float lng { get; set; }
+        }
+
+        public class Response
+        {
+            public string status { get; set; }
+            public string place_id { get; set; }
+            public string scope { get; set; }
+            public string reference { get; set; }
+            public string id { get; set; }
+        }
+
+    }
 }
